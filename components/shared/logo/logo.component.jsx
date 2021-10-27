53489cdbfd2a37844e037d89e3438e3b2c6fc81f
MGianetti <mateusgianetti1996@gmail.com>
2021-10-27 17:58:30 -0300

    Implemented navbar styles and Logo component

--- components/shared/logo/logo.component.jsx
+++ components/shared/logo/logo.component.jsx
@@ -1,5 +1,6 @@
 import styles from "./logo.module.scss";
+import Image from "next/image";
 
 export default function Logo() {
-  return <i>Brasvid</i>;
+  return <Image width={30} height={30} src="/BrasvidWhiteIcon.svg"></Image>;
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import styles from "./styles.scss";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
