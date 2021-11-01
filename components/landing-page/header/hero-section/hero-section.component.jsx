97fcfb19a8b83edc317c593190752457182498d0
MGianetti <mateusgianetti1996@gmail.com>
2021-11-01 19:21:49 -0300

    Implemented some styling

--- components/landing-page/header/hero-section/hero-section.component.jsx
+++ components/landing-page/header/hero-section/hero-section.component.jsx
@@ -3,9 +3,54 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <div className={styles.HeroSection}>
-      <Image src="/images/CurvedHeroGradient.png" layout="fill" />
-      <Image src="/images/WavesTransparency.png" layout="fill" />
-    </div>
+    <>
+      <div className={styles.HeroSection}>
+        <Image src="/images/CurvedHeroGradient.png" layout="fill" />
+        <Image src="/images/WavesTransparency.png" layout="fill" />
+        <div className={styles.HeroContent}>
+          <div className={styles.HeroText}>
+            <span className={styles.Brasvid}>Brasvid</span>
+            <span className={styles.BrasvidText}>
+              Vestibulum sollicitudin ipsum id mi tempus, sed semper tortor
+              eleifend. Donec mollis id arcu quis euismod.
+            </span>
+          </div>
+          <div className={styles.WindShield}>
+            <Image
+              src="/images/WindShield.png"
+              height={370}
+              width={477}
+              layout="fixed"
+            />
+          </div>
+          <div className={styles.WindShields}>
+            <div className={styles.WindShield}>
+              <Image
+                src="/images/WindShield.png"
+                height={370}
+                width={477}
+                layout="fixed"
+              />
+            </div>
+            <div className={styles.WindShield}>
+              <Image
+                src="/images/WindShield.png"
+                height={370}
+                width={477}
+                layout="fixed"
+              />
+            </div>
+            <div className={styles.WindShield}>
+              <Image
+                src="/images/WindShield.png"
+                height={370}
+                width={477}
+                layout="fixed"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
